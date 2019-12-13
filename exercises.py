fadcda30ef528e59a18b771efc9af360423cb943
Felix Lindblad <felixlindblad.fl@gmail.com>
2019-12-13 13:52:24 +0100

    4-6 är läxan

--- exercises.py
+++ exercises.py
@@ -28,3 +28,39 @@
     #return variable.upper() + "!!!" 
 
 #print(angry("ge mig min telefon"))
+
+#fråga 4 
+
+#my_list = ["tjäna", "hallå där"]
+#def copy_list(x):
+    #return x
+#print(copy_list(my_list))
+
+
+
+#fråga 5
+
+#string = input("stad: ")
+#lista = ["Stockholm", "New York", "Tokyo", "Berlin"]
+#def contains(lista, string):
+    #if string in lista:
+        #return True
+    #if string not in lista: 
+        #return False
+#print(contains(lista, string))
+
+
+#fråga 6
+
+#def divisble_by(o,y):
+    #if type(x) == int:
+        #return True
+    #else:
+        #if x.is_integer():
+            #return True
+        #else:
+            #return False
+#y = int(input("value: "))
+#o = int(input("value 2: "))
+#x = (y/o)
+#print(divisble_by(o, y))
